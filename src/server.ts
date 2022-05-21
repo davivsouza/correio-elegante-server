e69ed0ffeea4ea07dceeb594b663b96a20e9781c
davivsouza <davivasconcelossouza7@gmail.com>
2022-05-21 16:20:36 -0300

    deploy server

--- src/server.ts
+++ src/server.ts
@@ -5,4 +5,4 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(router);
-app.listen(4003, () => console.log("Server is runningon PORT 4003"));
+app.listen(process.env.PORT || 4003, () => console.log("Server is runningon PORT 4003"));
